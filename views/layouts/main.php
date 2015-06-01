f1ff0cb3114e9d3797444de6e333b3f15cfa3a66
Ekaterina <diademinna@yandex.ru>
2015-06-01 18:31:55 +0300

    sdsdas

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
-
+use app\assets\AppAsset;
+AppAsset::register($this);
 /* @var $this yii\web\View */
 /* @var $content string */
 ?>
@@ -14,12 +15,59 @@ use yii\helpers\Html;
         <?php $this->head() ?>
     </head>
     <body>
+    <header>
+        <div class="header_top">
+            <div class="content">
+                <div class="auth_menu">
+                    <ul class="menu">
+                        <li><a href="#">Регистрация</a></li>
+                        <li><a href="#">Личный кабинет</a></li>
+                    </ul>
+                </div>
+                <div class="my_cart">
+                    Моя корзина: 0 продуктов - 0 руб.
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="logo_container">
+            <div class="content">
+                <div class="logo">
+                    <a href="/"><img src="/img/logo.png" /></a>
+                </div>
+                <div class="block_contacts">
+                    <div class="item">Адрес: <span>г. Пенза, 1-й Онежский пр-д, д.6</span></div>
+                    <div class="item">Телефон: <span>8 937 000 23 32</span></div>
+                </div>
+                <div class="block_search">
+                    <form>
+                        <input type="text" value="" placeholder="Поиск..."/>
+                        <button></button>
+                        <div class="clear"></div>
+                    </form>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="main_menu">
+                <ul>
+                    <li><a href="#">Каталог</a></li>
+                    <li><a href="#">Доставка</a></li>
+                    <li><a href="#">Способы оплаты</a></li>
+                    <li><a href="#">Контакты</a></li>
+                    <li><a href="#">Акции</a></li>
+                    <li><a href="#">Отзывы клиентов</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
     <?php $this->beginBody() ?>
 
-
-    //
-
-    <?= $content ?>
+    <div class="content">
+        <?= $content ?>
+    </div>
 
     <?php $this->endBody() ?>
     </body>
--- views/site/index.php
+++ views/site/index.php
@@ -1,6 +1,6 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'Калейдоскоп';
 ?>
 
-index!!!
+hjghjgh
