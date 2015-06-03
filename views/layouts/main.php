bca405c801610b2e9ba0587658d4941dfce8e287
Ekaterina <diademinna@yandex.ru>
2015-06-03 14:19:49 +0300

    error in verstka

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
 use app\assets\AppAsset;
+
 AppAsset::register($this);
 /* @var $this yii\web\View */
 /* @var $content string */
@@ -15,26 +16,25 @@ AppAsset::register($this);
         <?php $this->head() ?>
     </head>
     <body>
-        <header>
-            <div class="top_menu">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <ul class="links">
-                                <li><a href="#">Регистрация</a></li>
-                                <li><a href="#">Личный кабинет</a></li>
-                            </ul>
-                            <div class="cart-position">
-                                <div id="cart">
-                                    <div class="heading">
-                                        <i class="icon-basket"></i>
-                                        Моя корзина: <span>(0) продуктов - 0 руб.</span>
-                                        <i class="icon-down-open"></i>
-                                    </div>
-                                    <div class="content">
-                                        <div class="content-scroll">
-                                            <div class="empty">Ваша корзина пуста!</div>
-                                        </div>
+    <header>
+        <div class="top_menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-12">
+                        <ul class="links">
+                            <li><a href="#">Регистрация</a></li>
+                            <li><a href="#">Личный кабинет</a></li>
+                        </ul>
+                        <div class="cart-position">
+                            <div id="cart">
+                                <div class="heading">
+                                    <i class="icon-basket"></i>
+                                    Моя корзина: <span>(0) продуктов - 0 руб.</span>
+                                    <i class="icon-down-open"></i>
+                                </div>
+                                <div class="content">
+                                    <div class="content-scroll">
+                                        <div class="empty">Ваша корзина пуста!</div>
                                     </div>
                                 </div>
                             </div>
@@ -42,47 +42,51 @@ AppAsset::register($this);
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="logo">
-                            <a href="/">
-                                <img alt="Интернет-магазин Калейдоскоп" title="Интернет-магазин Калейдоскоп" src="/img/logo.png">
-                            </a>
-                        </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-xs-12">
+                    <div class="logo">
+                        <a href="/">
+                            <img alt="Интернет-магазин Калейдоскоп" title="Интернет-магазин Калейдоскоп"
+                                 src="/img/logo.png">
+                        </a>
                     </div>
-                    <div class="col-sm-4">
-                        <div class="contacts_info">
-                            <div class="item">
-                                Адрес: <span>г. Пенза, 1-й Онежский пр-д, д.6</span>
-                            </div>
-                            <div class="item">
-                                Телефон: <span>8 800 523 65 35</span>
-                            </div>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="contacts_info">
+                        <div class="item">
+                            Адрес: <span>г. Пенза, 1-й Онежский пр-д, д.6</span>
                         </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="search">
-                            <form>
-                                <input type="text" placeholder="Поиск..." />
-                                <button><i class="icon-search"></i></button>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="col-sm-12">
-                        <div id="menu">
-                            <ul class="sf-menu">
-                                <li class="">
-                                    <a href="#">
-                                        <span>Congratulation</span>
-                                    </a>
-                                </li>
-                            </ul>
+                        <div class="item">
+                            Телефон: <span>8 800 523 65 35</span>
                         </div>
                     </div>
                 </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="search">
+                        <form>
+                            <input type="text" placeholder="Поиск..."/>
+                            <button><i class="icon-search"></i></button>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </header>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div id="menu">
+                        <ul class="sf-menu">
+                            <li class="">
+                                <a href="#">
+                                    <span>Congratulation</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
 
 
